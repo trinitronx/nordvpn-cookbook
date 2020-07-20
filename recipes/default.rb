4ef259847cfa3129cf4576aca756471af5eb9616
James Cuzella <james.cuzella@lyraphase.com>
2020-07-20 03:52:10 -0600

    Adding no-op default recipe

---
+++ recipes/default.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+#
+# Cookbook:: nordvpn
+# Recipe:: default
+# Author:: James Cuzella
+#
+# Copyright:: © 🄯 2020,  James Cuzella
+# License:: GPLv3.0
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# <
+# Add `nordvpn` users to `vpn` group.
+# >
+
+# if !node['nordvpn']['users'].nil? && !node['nordvpn']['users'].empty?
+#   group 'vpn' do
+#     action :modify
+#     members node['nordvpn']['users']
+#     system true
+#     append true
+#   end
+# end
